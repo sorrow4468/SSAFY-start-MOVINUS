ac1339ee790a2cd0b53fde88c5776fc662600237
이정원 <sorrow4468@gmail.com>
2021-11-18 15:53:33 +0900

    CORS충돌중

--- final-pjt-front/src/views/movies/MovieList.vue
+++ final-pjt-front/src/views/movies/MovieList.vue
@@ -1,14 +1,40 @@
 <template>
   <div>
-
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{ movie }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
-
+  name: 'MovieList',
+  data() {
+    return {
+      movies: null,
+    }
+  },
+  methods: {
+    getMovies: function() {
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/movies/'
+      })
+        .then(res => {
+          console.log(res)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+  },
+  created() {
+    this.getMovies()
+  }
 }
 </script>
 
