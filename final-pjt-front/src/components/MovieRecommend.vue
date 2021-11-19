d71ccc5a91ee1a972f5d70cc14a74ad2b5ac40ba
이정원 <sorrow4468@gmail.com>
2021-11-19 17:36:39 +0900

    장르 한국어 변환

--- final-pjt-front/src/components/MovieRecommend.vue
+++ final-pjt-front/src/components/MovieRecommend.vue
@@ -2,13 +2,16 @@
   <div>
     <h1>MovieRecommend</h1>
     <p>이 영화와 장르가 비슷한 영화들</p>
-    <template v-for="movie_genre in movie.genres">
-      <template v-for="genre in genres" v-if="movie_genre === genre[id]">
+    <!-- <template v-for="movie_genre in movie.genres">
+      <template v-for="genre in genres" v-if="movie_genre === genre.id">
       
         {{ genre.name }} 
         
-      </template>
-    </template>
+      </template> -->
+    <div v-for="findGenre in findGenres" :key="findGenre.id">
+      {{ findGenre }}  
+    </div>
+    <!-- </template> -->
   </div>
 </template>
 
@@ -17,12 +20,33 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'MovieRecommend',
+  data() {
+    return {
+      findGenres: []  
+    }
+  },
   computed: {
     ...mapState([
       'movie',
       'imgSrc',
       'genres',
-    ])
+    ]),
+  },
+  methods: {    
+    findGenre() {
+      this.genres.forEach(genre => {
+        this.movie.genres.forEach(movie_genre => {
+          if (movie_genre === genre['id']) {
+            // console.log(genre['name'])
+            this.findGenres.push(genre['name'])
+          }
+        })
+      })
+      return this.findGenres
+    }
+  },
+  created() {
+    this.findGenre()
   }
 }
 </script>
