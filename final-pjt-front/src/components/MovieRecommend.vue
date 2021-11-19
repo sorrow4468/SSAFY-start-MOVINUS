f97c0f7550b2bab9f8b4b7c6e7760c35118d31bf
이정원 <sorrow4468@gmail.com>
2021-11-19 14:46:22 +0900

    쉬는시간 끝

--- final-pjt-front/src/components/MovieRecommend.vue
+++ final-pjt-front/src/components/MovieRecommend.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <h1>MovieRecommend</h1>
+    <p>이 영화와 장르가 비슷한 영화들</p>
+
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
 
+export default {
+  name: 'MovieRecommend',
+  computed: {
+    ...mapState([
+      'movie',
+      'imgSrc',
+    ])
+  }
 }
 </script>
 
