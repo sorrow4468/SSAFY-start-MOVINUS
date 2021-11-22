aff19c75c35b7fdf2fa5694b9a826d9b89db673f
정윤정 <yoonjung1205@naver.com>
2021-11-22 12:57:42 +0900

    review 수정 중

---
+++ final-pjt-front/src/components/community/Comments.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Comments</h1>
+    <comment-form></comment-form>
+    <comments-item></comments-item>
+  </div>
+</template>
+
+<script>
+import CommentForm from '@/components/community/CommentForm'
+import CommentsItem from '@/components/community/CommentsItem'
+
+export default {
+  name:'Comment',
+  components: {
+    CommentForm,
+    CommentsItem,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/components/community/ReviewsItem.vue
+++ final-pjt-front/src/components/community/ReviewsItem.vue
@@ -8,17 +8,30 @@
     <p>작성시간: {{review.created_at}}</p>
     <p>수정시간: {{review.updated_at}}</p>
     <p>별점: {{review.rank}}</p>
+    <button class="btn btn-success" @click="updateReview(review)">수정</button>
+    <button class="btn btn-danger" @click="deleteReview(review)">삭제</button>
+    <Comments/>
   </div>
+
+
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import Comments from '@/components/community/Comments'
+
 export default {
+  components: { Comments },
   name: 'ReviewsItem',
   computed:{
     ...mapState([
       'review',
     ])
+  },
+  mathods: {
+    // updatdeReview() {
+    //   const 
+    // }
   }
 }
 </script>
