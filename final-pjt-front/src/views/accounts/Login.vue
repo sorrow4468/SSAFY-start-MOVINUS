3163c76cfc9fd27e0874d52437323ad5bf80caf3
이정원 <sorrow4468@gmail.com>
2021-11-25 15:31:16 +0900

    리뷰 데이터 추가

--- final-pjt-front/src/views/accounts/Login.vue
+++ final-pjt-front/src/views/accounts/Login.vue
@@ -3,7 +3,7 @@
     <h1 style="padding-top:220px;">로그인</h1>
     <div class="d-flex justify-content-center">
       <b-form style="width:400px;">        
-        <b-form-input class="rounded-pill mt-3" style="height:50px;" id="username" v-model="credentials.username" required placeholder="아이디"></b-form-input>
+        <b-form-input class="rounded-pill mt-3" style="height:50px;" id="username" v-model="credentials.username" required placeholder="아이디" @keyup.enter="login"></b-form-input>
         <b-form-input class="rounded-pill mt-3" style="height:50px;" v-model="credentials.password" type="password" id="password" aria-describedby="password-help-block" placeholder="비밀번호" @keyup.enter="login"></b-form-input>
         <b-button class="rounded-pill mt-3 w-100" style="height:50px;" @click="login" variant="primary">로그인</b-button>
         <p class="m-3">로그인 후 메인페이지로 이동합니다</p>
